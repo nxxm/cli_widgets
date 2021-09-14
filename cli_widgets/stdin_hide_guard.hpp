771b597bfe80b319b7f1ad74b17f1abf0734044a
Lambourl <lmbrluc@gmail.com>
2021-09-14 13:05:41 +0200

    :wrench: use a struct to to respect the raii

--- cli_widgets/stdin_hide_guard.hpp
+++ cli_widgets/stdin_hide_guard.hpp
@@ -8,44 +8,36 @@
 #include <unistd.h>
 #endif
 
-namespace cli_widgets{
-  struct stdin_hide_guard {
-    stdin_hide_guard(bool enable) {
+namespace cli_widgets {
+
+  inline void set_stdin_echo(bool enable) {
 #if BOOST_OS_WINDOWS
-     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-     DWORD mode;
-     GetConsoleMode(hStdin, &mode);
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
 
-     if (!enable){
-       mode &= ~ENABLE_ECHO_INPUT;
-     }else{
-       mode |= ENABLE_ECHO_INPUT;}
-
-     SetConsoleMode(hStdin, mode);
- #else
-     struct termios tty;
-     tcgetattr(STDIN_FILENO, &tty);
-     if (!enable){
-       tty.c_lflag &= ~ECHO;
-     }else{
-       tty.c_lflag |= ECHO;}
-     (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-#endif
-    }
-
-    ~stdin_hide_guard() {
-#if BOOST_OS_WINDOWS
-      HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-      DWORD mode;
-      GetConsoleMode(hStdin, &mode);
+    if (!enable) {
+      mode &= ~ENABLE_ECHO_INPUT;
+    } else {
       mode |= ENABLE_ECHO_INPUT;
-      SetConsoleMode(hStdin, mode);
-#else
-      struct termios tty;
-      tcgetattr(STDIN_FILENO, &tty);
-      tty.c_lflag |= ECHO;
-      (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-#endif
     }
+
+    SetConsoleMode(hStdin, mode);
+#else
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    if (!enable) {
+      tty.c_lflag &= ~ECHO;
+    } else {
+      tty.c_lflag |= ECHO;
+    }
+
+    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+#endif
+  }
+  
+  struct stdin_hide_guard {
+    stdin_hide_guard(bool enable) { set_stdin_echo(enable); }
+    ~stdin_hide_guard() { set_stdin_echo(false); }
   };
 }
