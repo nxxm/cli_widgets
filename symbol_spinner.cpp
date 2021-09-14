c8856b2620ca63829b0628196f878382c999dca4
Yannic Staudt <yannic@staudt-engineering.com>
2021-09-14 17:04:09 +0200

    Added cli_widgets::symbol_spinner and a demo

---
+++ symbol_spinner.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+#include <thread>
+#include <chrono>
+#include <cli_widgets/spin_loader.hpp>
+#include <cli_widgets/symbol_spinner.hpp>
+#include <termcolor/termcolor.hpp>
+
+const std::chrono::nanoseconds step_slow{100000000};
+
+int main(int argc, char** argv) {
+
+  std::cout << termcolor::yellow << "Just a simple symblo spinner: " << termcolor::reset << std::endl;
+
+  const std::list<std::string> symbols { "v", "<", "^", ">" };
+  cli_widgets::symbol_spinner spinner(symbols);
+
+  for(int i = 50; i > 0; i--) {
+    std::cout << "\r" << spinner.next() << " <- Hey! Look at me =D" << std::flush;
+    std::this_thread::sleep_for(step_slow);
+  }
+
+  std::cout << std::endl;
+  std::cout << termcolor::yellow << "Same game using one of the inbuilt symbol collections: " << termcolor::reset << std::endl;
+
+  cli_widgets::symbol_spinner spinner_inbuilt(cli_widgets::spinner_symbols::symbols_braille_inverted);
+
+  for(int i = 50; i > 0; i--) {
+    std::cout << "\r" << termcolor::bright_red << spinner_inbuilt.next() << termcolor::reset;
+    std::cout << " <- We can do just the spinner in color as well!" << std::flush;
+    std::this_thread::sleep_for(step_slow);
+  }
+
+  std::cout << std::endl;
+  std::cout << "Combining ";
+  std::cout << termcolor::bright_magenta << "cli_widget::symbol_spinner" << termcolor::reset;
+  std::cout << " and ";
+  std::cout << termcolor::cyan << "cli_widget::spin_loader" << termcolor::reset;
+  std::cout << std::endl;
+
+  // Combining two widgets
+  cli_widgets::symbol_spinner spinner_inbuilt2(cli_widgets::spinner_symbols::symbols_bars_horizontal);
+  cli_widgets::spin_loader loader(std::cout);
+  loader.launch();
+
+  for(int i = 100; i > 0; i--) {
+    std::cout << " --> " << termcolor::bright_green << spinner_inbuilt2.next() << termcolor::reset;
+    std::cout << " <-- totally magically works too";
+    std::this_thread::sleep_for(step_slow);
+  }
+
+  loader.stop();
+  return 0;
+}
