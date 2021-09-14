dc72e99bedccb0d43a4b6929fbaa9511f77770ca
Lambourl <lmbrluc@gmail.com>
2021-09-14 12:35:43 +0200

    :wrench: add constructor with bool

--- cli_widgets/stdin_hide_guard.hpp
+++ cli_widgets/stdin_hide_guard.hpp
@@ -10,18 +10,26 @@
 
 namespace cli_widgets{
   struct stdin_hide_guard {
-    stdin_hide_guard() {
+    stdin_hide_guard(bool enable) {
 #if BOOST_OS_WINDOWS
-      HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-      DWORD mode;
-      GetConsoleMode(hStdin, &mode);
-      mode &= ~ENABLE_ECHO_INPUT;
-      SetConsoleMode(hStdin, mode);
-#else
-      struct termios tty;
-      tcgetattr(STDIN_FILENO, &tty);
-      tty.c_lflag &= ~ECHO;
-      (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+     HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+     DWORD mode;
+     GetConsoleMode(hStdin, &mode);
+
+     if (!enable){
+       mode &= ~ENABLE_ECHO_INPUT;
+     }else{
+       mode |= ENABLE_ECHO_INPUT;}
+
+     SetConsoleMode(hStdin, mode);
+ #else
+     struct termios tty;
+     tcgetattr(STDIN_FILENO, &tty);
+     if (!enable){
+       tty.c_lflag &= ~ECHO;
+     }else{
+       tty.c_lflag |= ECHO;}
+     (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
 #endif
     }
 
