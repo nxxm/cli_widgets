a753a937175ca55e025b3120df7ee90ab22c55ed
Damien Buhl <damien.buhl@lecbna.org>
2021-09-15 16:05:13 +0200

    :lipstick: show that it's thought to work as A RAII guard

--- examples/stdin_hide_guard.cpp
+++ examples/stdin_hide_guard.cpp
@@ -6,8 +6,12 @@
 int main(int argc, char **argv) {
   std::string passphrase;
   std::cout << "Please enter your password : " << std::endl;
-  cli_widgets::stdin_hide_guard hide_password{};
-  std::cin >> passphrase;
+  
+  {
+    cli_widgets::stdin_hide_guard hide_password{};
+    std::cin >> passphrase;
+  }
+  
   std::cout << "passphrase : " << passphrase << std::endl;
   return 0;
 }
